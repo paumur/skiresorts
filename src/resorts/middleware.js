4344d9c23f0148968edd1c944320aead45a81523
Paulius Murasovas <pauliusmurasovas@gmail.com>
2023-02-15 20:07:09 +0200

    fixed middleware.

--- src/resorts/middleware.js
+++ src/resorts/middleware.js
@@ -1,21 +1,13 @@
 import * as types from '../resorts/types';
 
 const myMiddleware = (store) => (next) => (action) => {
+  next(action);
   if (action.type === types.ADD_OR_REMOVE_FAVORITES) {
-    if (store.getState().resorts.favorites.includes(action.payload)) {
-      const index = store.getState().resorts.favorites.indexOf(action.payload);
-      const favorites = [...store.getState().resorts.favorites];
-      favorites.splice(index, 1);
-      localStorage.setItem('favorites', JSON.stringify(favorites));
-    } else {
-      const newFavorites = [
-        ...store.getState().resorts.favorites,
-        action.payload,
-      ];
-      localStorage.setItem('favorites', JSON.stringify(newFavorites));
-    }
+    localStorage.setItem(
+      'favorites',
+      JSON.stringify(store.getState().resorts.favorites)
+    );
   }
-  return next(action);
 };
 
 export default myMiddleware;
